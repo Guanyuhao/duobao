ffd364b4561f1c8320a4f24a92d2676747b8f6a1
Guanyuhao <272949902@qq.com>
2017-03-03 10:11:21 +0800

    111

---
+++ duobaoWeb/js/car.js
@@ -0,0 +1,91 @@
+//这里我做了必须参数userId 跟 token的处理
+function addcar(opt){
+		if(localStorage.user){
+			//确保有用户 才能对其进行处理
+			var User = JSON.parse(localStorage.user);
+			var UserInfo = User.userInfo;
+			var config = {
+				goodsSaleId:'',
+				userId:UserInfo.userId,
+				token:UserInfo.caibaoToken,
+				callback:function(res){
+					
+				}
+			}
+			for( var i in opt ){
+				config[i] = opt[i]
+			}
+			var Url ='?goodsSaleId='+config.goodsSaleId+'&userId='+config.userId+'&token='+config.token;
+			jsajax({
+				method:'post',
+				url:'http://139.129.203.90/caibao/car/save'+Url,
+				success:function(res){
+					config.callback(res)
+				}
+			})
+			
+		}else{
+			mui.confirm('您好，请你先登录',' ',['确认','取消'],function(e){
+			 	//console.log(e.index)
+			 	if(e.index == 0){
+					mui.openWindow({
+						url: 'login.html',
+						id:'login.html'
+					})
+			 	}
+			 },'div');	
+		}
+	}
+//删除
+function delcar(opt){
+	if(localStorage.user){
+		var User = JSON.parse(localStorage.user);
+		var UserInfo = User.userInfo;
+		var config = {
+			carIds:'',
+			userId:UserInfo.userId,
+			token:UserInfo.caibaoToken,
+			callback:function(res){
+				
+			}
+		}
+		for( var i in opt ){
+			config[i] = opt[i]
+		}
+		var Url ='?carIds='+config.carIds+'&userId='+config.userId+'&token='+config.token;
+		jsajax({
+			method:'post',
+			url:'http://139.129.203.90/caibao/car/delBatch'+Url,
+			success:function(res){
+				console.log(res)
+				config.callback(res)
+			}
+		})
+	}
+}
+function numupdate(opt){
+	if(localStorage.user){
+		var User = JSON.parse(localStorage.user);
+		var UserInfo = User.userInfo;
+		var config = {
+			num:'',
+			itemId:'',
+			userId:UserInfo.userId,
+			token:UserInfo.caibaoToken,
+			callback:function(res){
+				
+			}
+		}
+		for( var i in opt ){
+			config[i] = opt[i]
+		}
+		var Url ='?num='+config.num+'&itemId='+config.itemId+'&userId='+config.userId+'&token='+config.token;
+		jsajax({
+			method:'post',
+			url:'http://139.129.203.90/caibao/car/num'+Url,
+			success:function(res){
+				config.callback(res)
+			}
+		})
+	}
+}
---
+++ duobaoWeb/js/footer.js
@@ -0,0 +1,63 @@
+
+mui('footer').on('tap','a',function(){
+	var _this =  this;
+	if(this.getAttribute('off')){
+		
+		//需要登录权限
+		if(localStorage.getItem('user')){
+			//还得确认一次是否被顶了
+			var userInfo =  JSON.parse(localStorage.getItem('user')).userInfo;
+			var Url = '?userId='+userInfo.userId+'&token='+userInfo.caibaoToken;
+			jsajax({
+				method:'post',
+				url:'http://139.129.203.90/caibao/car/list'+Url,
+				success:function(res){
+					var User = JSON.parse(res);
+					if(User.code == 0){
+						mui.openWindow({
+							url: _this.href,
+							id:_this.href
+					})
+					}else if(User.code == 777){
+						localStorage.user = '';
+						mui.confirm('你在其他地方登录了',' ',['确认','取消'],function(e){
+						 	if(e.index == 0){
+								mui.openWindow({
+									url: 'login.html',
+									id:'login.html'
+								})
+						 	}
+						 },'div');
+					}
+				}
+			})
+			
+			
+			
+		}else{
+			//没有登录
+			
+		 mui.confirm('您好，请你先登录',' ',['确认','取消'],function(e){
+		 	//console.log(e.index)
+		 	if(e.index == 0){
+					mui.openWindow({
+						url: 'login.html',
+						id:'login.html'
+					})
+		 	}
+		 },'div');		
+//			plus.nativeUI.confirm( "您好,请您先登录", function(e){
+//				if(e.index == 0){
+//						document.location.href='login.html';	
+//				 	}
+//			}, "提示", ["确认","取消"]);
+		}
+	}else{
+		
+		mui.openWindow({
+			url: _this.href,
+			id:_this.href
+		})			
+	}
+
+});
